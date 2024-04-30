4217b66d1ed57970b02916bfa5703f238a467285
Nanaanaaa <3516892226@qq.com>
2024-04-30 16:48:10 +0800

    1

---
+++ sieve.py
@@ -0,0 +1,17 @@
+minp, primes = [], []
+
+
+def sieve(N: int):
+    minp = [0] * (N + 1)
+    primes = []
+    for i in range(2, N + 1):
+        if minp[i] == 0:
+            minp[i] = i
+            primes.append(i)
+        for p in primes:
+            if i * p > N:
+                break
+            minp[i * p] = p
+            if p == minp[i]:
+                break
+    return
