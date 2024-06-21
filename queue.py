1db49ed55cb331be112f76969cfae87369542437
Nanaanaaa <3516892226@qq.com>
2024-06-21 10:02:51 +0800

    1

--- queue.py
+++ queue.py
@@ -1,12 +1,45 @@
 class queue:
     def __init__(self):
-        self.q = []
-        self.n = 0
-        self.hh = 0
-        self.tt = -1
+        self.q = [None]
+        self.capacity = 1
+        self.siz = 0
+        self.head = 0
+        self.rear = -1
+
+    def empty(self):
+        return self.siz == 0
+
+    def size(self):
+        return self.siz
+
+    def resize(self, capacity):
+        q = [None] * capacity
+        for i in range(self.siz):
+            q[i] = self.q[(self.head + i) % self.capacity]
+        self.q = q
+        self.front = 0
+        self.rear = self.siz - 1
+        self.capacity = capacity
+        return
+
+    def push(self, x):
+        if self.siz == self.capacity:
+            self.resize(2 * self.capacity)
+        self.rear = (self.rear + 1) % self.capacity
+        self.q[self.rear] = x
+        self.siz += 1
+        return
+
     def pop(self):
-        assert self.n != 0
-        self.hh = (self.hh - 1 + self.n) % self.n
-        assert self.hh != self.tt
-    def push(self):
-        
+        if not self.empty():
+            res = self.q[self.head]
+            self.head = (self.head + 1) % self.capacity
+            self.siz -= 1
+            return res
+        raise IndexError("queue is empty")
+
+    def front(self):
+        return self.q[self.head]
+
+    def back(self):
+        return self.q[self.rear]
