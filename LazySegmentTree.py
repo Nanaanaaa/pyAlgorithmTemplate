5b879a7cf7f692132f19b15f3e885fccfcdfc8d4
Nanaanaaa <3516892226@qq.com>
2025-03-04 19:13:09 +0800

    seg

--- LazySegmentTree.py
+++ LazySegmentTree.py
@@ -1,26 +1,40 @@
-from typing import Generic, List, TypeVar, Callable
+import sys, math, string
+from collections import defaultdict
+from bisect import bisect_right as upper_bound, bisect_left as lower_bound
 
-Info = TypeVar("Info")
-Tag = TypeVar("Tag")
+input = lambda: sys.stdin.readline().rstrip()
+read = lambda: int(input())
+reads = lambda: list(map(int, input().split()))
 
 
-class LazySegmentTree(Generic[Info, Tag]):
-    def __init__(self, data: List[Info] = None, size: int = 0):
-        if data is None:
-            data = [Info()] * size
+class LazySegmentTree:
+    def __init__(
+        self,
+        data=None,
+        size=0,
+        Info=None,
+        Tag=None,
+    ):
+        self.Info = Info
+        self.Tag = Tag
+        self.infoApply = infoApply
+        self.tagApply = tagApply
+        self.add = add
 
         self.n = len(data)
         self.size = 1 << ((self.n - 1).bit_length() if self.n > 0 else 0)
         self.log = self.size.bit_length() - 1 if self.size > 0 else 0
 
-        self.info: List[Info] = [Info()] * (2 * self.size)
-        self.info[self.size : self.size + self.n] = data
+        self.info = [self.Info] * (2 * self.size)
+        self.info[self.size : self.size + self.n] = (
+            data if data is not None else [Info] * size
+        )
 
-        self.tag = [Tag() for _ in range(self.size)]
+        self.tag = [self.Tag] * self.size
         for i in range(self.size - 1, 0, -1):
             self.pull(i)
 
-    def modify(self, p: int, value: Info) -> None:
+    def modify(self, p: int, value) -> None:
         p += self.size
         for i in range(self.log, 0, -1):
             self.push(p >> i)
@@ -29,19 +43,19 @@ class LazySegmentTree(Generic[Info, Tag]):
         for i in range(1, self.log + 1):
             self.pull(p >> i)
 
-    def __getitem__(self, p: int) -> Info:
+    def __getitem__(self, p: int):
         p += self.size
         for i in range(self.log, 0, -1):
             self.push(p >> i)
         return self.info[p]
 
-    def query(self, l: int, r: int) -> Info:
+    def query(self, l: int, r: int):
         if l == r:
-            return Info()
+            return self.Info
         l += self.size
         r += self.size
-        res_left = Info()
-        res_right = Info()
+        res_left = self.Info
+        res_right = self.Info
 
         for i in range(self.log, 0, -1):
             if (l >> i << i) != l:
@@ -51,19 +65,19 @@ class LazySegmentTree(Generic[Info, Tag]):
 
         while l < r:
             if l & 1:
-                res_left = res_left + self.info[l]
+                res_left = self.add(res_left, self.info[l])
                 l += 1
             if r & 1:
                 r -= 1
-                res_right = self.info[r] + res_right
+                res_right = self.add(self.info[r], res_right)
             l >>= 1
             r >>= 1
-        return res_left + res_right
+        return self.add(res_left, res_right)
 
-    def __call__(self, l: int, r: int) -> Info:
+    def __call__(self, l: int, r: int):
         return self.query(l, r)
 
-    def rangeApply(self, l: int, r: int, t: Tag) -> None:
+    def rangeApply(self, l: int, r: int, t):
         if l == r:
             return
 
@@ -93,86 +107,89 @@ class LazySegmentTree(Generic[Info, Tag]):
             if (r >> i << i) != r:
                 self.pull((r - 1) >> i)
 
-    def maxRight(self, l: int, pred: Callable[[Info], bool]) -> int:
+    def maxRight(self, l: int, pred) -> int:
         if l == self.n:
             return self.n
         l += self.size
         for i in range(self.log, 0, -1):
             self.push(l >> i)
 
-        cur = Info()
+        cur = self.Info
 
         while True:
             while l % 2 == 0:
                 l >>= 1
-            if not pred(cur + self.info[l]):
+            if not pred(self.add(cur, self.info[l])):
                 while l < self.size:
                     self.push(l)
                     l <<= 1
-                    if pred(cur + self.info[l]):
-                        cur = cur + self.info[l]
+                    if pred(self.add(cur, self.info[l])):
+                        cur = self.add(cur, self.info[l])
                         l += 1
                 return l - self.size
-            cur = cur + self.info[l]
+            cur = self.add(cur, self.info[l])
             l += 1
             if (l & -l) == l:
                 break
         return self.n
 
-    def minLeft(self, r: int, pred: Callable[[Info], bool]) -> int:
+    def minLeft(self, r: int, pred) -> int:
         if r == 0:
             return 0
         r += self.size
         for i in range(self.log, 0, -1):
             self.push((r - 1) >> i)
 
-        cur = Info()
+        cur = self.Info
 
         while True:
             r -= 1
             while r > 1 and (r & 1):
                 r >>= 1
-            if not pred(self.info[r] + cur):
+            if not pred(self.add(self.info[r], cur)):
                 while r < self.size:
                     self.push(r)
                     r = 2 * r + 1
-                    if pred(self.info[r] + cur):
-                        cur = self.info[r] + cur
+                    if pred(self.add(self.info[r], cur)):
+                        cur = self.add(self.info[r], cur)
                         r -= 1
                 return r + 1 - self.size
-            cur = self.info[r] + cur
+            cur = self.add(self.info[r], cur)
             if (r & -r) == r:
                 break
         return 0
 
-    def apply(self, p: int, t: Tag) -> None:
-        self.info[p].apply(t)
+    def apply(self, p: int, t) -> None:
+        self.info[p] = self.infoApply(self.info[p], t)
         if p < self.size:
-            self.tag[p].apply(t)
+            self.tag[p] = self.tagApply(self.tag[p], t)
 
     def pull(self, p: int) -> None:
-        self.info[p] = self.info[2 * p] + self.info[2 * p + 1]
+        self.info[p] = self.add(self.info[2 * p], self.info[2 * p + 1])
 
     def push(self, p: int) -> None:
         self.apply(2 * p, self.tag[p])
         self.apply(2 * p + 1, self.tag[p])
-        self.tag[p] = Tag()
+        self.tag[p] = self.Tag
 
 
-class Tag:
-    def __init__(self) -> None:
-        pass
-
-    def apply(self, t: Tag) -> None:
-        pass
+def tagApply(a, b):
+    l_b, l_c = a
+    r_b, r_c = b
+    return (l_b * r_b % P, (l_c * r_b + r_c) % P)
 
 
-class Info:
-    def __init__(self, ans: int = 0) -> None:
-        pass
+def infoApply(l, r):
+    ans, siz = l
+    b, c = r
+    return ((ans * b + c * siz) % P, siz)
 
-    def apply(self, t: Tag) -> None:
-        pass
 
-    def __add__(self, other: Info) -> None:
-        return Info()
+def add(a, b):
+    l_ans, l_siz = a
+    r_ans, r_siz = b
+    return ((l_ans + r_ans) % P, l_siz + r_siz)
+
+
+Info = (0, 1)
+Tag = (1, 0)
